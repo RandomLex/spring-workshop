eaa84bfa5099992b32e4eb55ea8ff69f0a519eeb
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-02-24 21:40:17 +0300

    p/c/util

--- src/main/java/by/academy/spring/model/Student.java
+++ src/main/java/by/academy/spring/model/Student.java
@@ -7,23 +7,24 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class Student extends Person {
-    private List<Integer> marks;
+    private Map<String, Integer> marks;
 
-    public Student(int id, String name) {
-        super(id, name);
-    }
-
-    public Student(int id, String name, List<Integer> marks) {
-        super(id, name);
-        this.marks = marks;
-    }
+//    public Student(int id, String name) {
+//        super(id, name);
+//    }
+//
+//    public Student(int id, String name, List<Integer> marks) {
+//        super(id, name);
+//        this.marks = marks;
+//    }
 
     @Override
     public void print(String str) {
